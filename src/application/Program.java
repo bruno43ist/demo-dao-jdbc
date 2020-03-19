effbefe2bfff7fd2e234ec7bbe41c34aef91b795
Bruno Farias <brunofarias@MacBook-Pro-de-Bruno.local>
2020-03-19 17:02:50 -0300

    classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller sel = new Seller(1, "Alex", "alex@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(obj);
+		System.out.println(sel);
 
 	}
 
